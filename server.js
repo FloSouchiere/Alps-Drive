bf400ecf8539047b8d0ee11f47fa06471e6b33d3
MBP <onin@MacBook-Pro-de-MBP.local>
2020-04-17 09:37:15 +0200

    Push correction exo 5

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 const express = require("express")
 const app = express()
 
-app.use('/', express.static("./frontend/JS_alps-drive-project-frontend"))
-app.get('/', function (req, res) {
+app.use(express.static("frontend"))
 
-    res.setHeader("Content-Type", "text/plain")
-    res.send("Vous êtes à l\'accueil")
-})
+module.exports = {
+    start: () => {
+        app.listen(3000, () => {
+            console.log("lListening on http://localhost:3000")
+        })
 
-app.use(function (req, res, next) {
-
-    res.setHeader("Confert-Type", "text/plain")
-    res.status(404).send("404 Not Found !")
-})
-
-function start() {
-    app.listen(3000, function () {
-        console.log("Balance ton port 3000")
-    })
-}
-
-module.exports = {start}
+    }}
